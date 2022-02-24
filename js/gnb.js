a9b4aa5bf9cccbc73bfd3e7e135cf7ea40794c11
DESKTOP-9PI9TTC\a <lhm820107@gmail.com>
2022-02-24 17:39:52 +0900

    gnb.js 수정중

--- js/gnb.js
+++ js/gnb.js
@@ -1,6 +1,9 @@
 const gnb = document.querySelector('.global-header .gnb');
-const gnbNav = document.querySelector('.gnb-nav')
+const gnbNav = document.querySelector('.gnb-nav');
+const gnbNavList = document.querySelector('.gnb-nav-list');
+
 
 gnbNav.addEventListener('mouseenter',function(){
-  gnb.classList.add('is-open')
+  gnb.classList.add('is-open');
+  gnbNavList.classList.add('is-open');
 })
