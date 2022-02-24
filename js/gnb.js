435f66de6951255aa26ce9c008f3eb14ffd65120
DESKTOP-N4V80RO\lhm0107 <lhm820107@gmail.com>
2022-02-24 23:08:52 +0900

    💄 gnb 메뉴 hover 및 nav mouseenter , mouseleave 효과 js작성

--- js/gnb.js
+++ js/gnb.js
@@ -1,9 +1,21 @@
 const gnb = document.querySelector('.global-header .gnb');
-const gnbNav = document.querySelector('.gnb-nav');
 const gnbNavList = document.querySelector('.gnb-nav-list');
+const gnbNavWrap = document.querySelector('.gnb-nav-wrap');
 
+function gnbNavOpen() {
+	gnb.classList.add('is-open');
+	gnbNavList.classList.add('is-open');
+	gnbNavWrap.classList.add('is-open');
+}
 
-gnbNav.addEventListener('mouseenter',function(){
-  gnb.classList.add('is-open');
-  gnbNavList.classList.add('is-open');
-})
+gnbNavWrap.addEventListener('mouseenter', gnbNavOpen);
+
+function gnbNavClose() {
+	gnbNavList.classList.remove('is-open');
+	gnbNavWrap.classList.remove('is-open');
+	gnb.classList.remove('is-open');
+}
+
+const gnbNavWrapIsOpen = document.querySelector('.gnb-nav-wrap.is-open');
+
+gnbNavWrap.addEventListener('mouseleave', gnbNavClose);
