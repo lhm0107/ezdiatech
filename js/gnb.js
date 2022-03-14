48531924c226879b52053781d6ebb050716e46b8
DESKTOP-9PI9TTC\a <lhm820107@gmail.com>
2022-03-14 17:37:42 +0900

    🚧 add gnb scroll effect

--- js/gnb.js
+++ js/gnb.js
@@ -1,11 +1,14 @@
 const gnb = document.querySelector('.global-header .gnb');
 const gnbNavList = document.querySelector('.gnb-nav-list');
 const gnbNavWrap = document.querySelector('.gnb-nav-wrap');
+const globalHeader = document.querySelector('.global-header');
 
 function gnbNavOpen() {
 	gnb.classList.add('is-open');
 	gnbNavList.classList.add('is-open');
 	gnbNavWrap.classList.add('is-open');
+	gnb.classList.remove('is-top');
+	globalHeader.classList.remove('is-top');
 }
 
 gnbNavWrap.addEventListener('mouseenter', gnbNavOpen);
@@ -14,8 +17,22 @@ function gnbNavClose() {
 	gnbNavList.classList.remove('is-open');
 	gnbNavWrap.classList.remove('is-open');
 	gnb.classList.remove('is-open');
+	gnb.classList.add('is-top');
+	globalHeader.classList.add('is-top');
 }
 
 const gnbNavWrapIsOpen = document.querySelector('.gnb-nav-wrap.is-open');
 
 gnbNavWrap.addEventListener('mouseleave', gnbNavClose);
+
+document.addEventListener('scroll', function(){
+	let mainScrollHeight = window.scrollY
+	if (mainScrollHeight == 0) {
+		gnb.classList.add('is-top');
+	globalHeader.classList.add('is-top');
+	} else if (mainScrollHeight > 0) {
+			gnb.classList.remove('is-top');
+	globalHeader.classList.remove('is-top');
+	}
+})
+
